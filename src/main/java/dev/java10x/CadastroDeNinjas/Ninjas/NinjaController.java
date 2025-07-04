14b18b761f4f06c015a9b01ab32a50c6cdfd7fb8
Iago Zancanaro <iagozancanaro@gmail.com>
2025-07-03 21:37:37 -0300

    feat: Atualiza o controller com o mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -10,4 +10,34 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
     }
+
+    // Adcionar ninja (Create)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // Mostra todos os ninjas (Read)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar todos os ninjas";
+    }
+
+    // Mostra ninja por ID (Read)
+    @GetMapping("/todosID")
+    public String mostrarNinjasPorId() {
+        return "Mostrar ninjas por ID";
+    }
+
+    // Alterar dodos dos ninjas (Update)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar ninja por ID";
+    }
+
+    // Deletar ninja (Delete)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Deletar ninja por ID";
+    }
 }
